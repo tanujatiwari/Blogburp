4d4e8b3ab7bab706ee6eeffffe196cac856f9fad
Tanuja <tanujatiwari04@gmail.com>
2021-08-15 02:18:41 +0530

    add posted by line

--- routes/poetry/comments.js
+++ routes/poetry/comments.js
@@ -1,98 +1,98 @@
-var express= require("express");
-var router  = express.Router();
+var express = require("express");
+var router = express.Router();
 var Poetry = require("../../models/poetry");
 var Comment = require("../../models/comment");
 var middleware = require("../../middleware")
 
 
 //NEW	
-	router.get("/poetry/:id/comments/new",middleware.isLoggedIn,function(req,res){
-		Poetry.findById(req.params.id,function(err,poem){
-			if(err)
-				req.flash("error","Something went wrong...Please try again later!");
-			else
-				res.render("comments/poetry/new",{poetry : poem});
-		});
+router.get("/poetry/:id/comments/new", middleware.isLoggedIn, function (req, res) {
+	Poetry.findById(req.params.id, function (err, poem) {
+		if (err)
+			req.flash("error", "Something went wrong...Please try again later!");
+		else
+			res.render("comments/poetry/new", { poetry: poem });
 	});
+});
 
-	router.post("/poetry/:id/comments",middleware.isLoggedIn, function(req,res){
-		Poetry.findById(req.params.id,function(err,poem){
-			if(err){
-				req.flash("error","Something went wrong...Please try again later!");
-				res.redirect("/poetry");
-			}
-			else{
-				//create new comment
-				Comment.create(req.body.comment,function(err,comment){
-					if(err)
-						req.flash("error","Something went wrong...Please try again later!");
-					else{
-						//add username and id to that comment
-						comment.author.id = req.user._id;
-						comment.author.username = req.user.username;
-						comment.save();
-						//save comment
-						poem.comments.push(comment);
-						poem.save();
-						req.flash("success","Successfully added a comment! Thanks for the review <3 ")
-						res.redirect("/poetry/" + poem._id);
-					}
-				});
-			}
-		});
+router.post("/poetry/:id/comments", middleware.isLoggedIn, function (req, res) {
+	Poetry.findById(req.params.id, function (err, poem) {
+		if (err) {
+			req.flash("error", "Something went wrong...Please try again later!");
+			res.redirect("/poetry");
+		}
+		else {
+			//create new comment
+			Comment.create(req.body.comment, function (err, comment) {
+				if (err)
+					req.flash("error", "Something went wrong...Please try again later!");
+				else {
+					//add username and id to that comment
+					comment.author.id = req.user._id;
+					comment.author.username = req.user.username;
+					comment.save();
+					//save comment
+					poem.comments.push(comment);
+					poem.save();
+					req.flash("success", "Successfully added a comment! Thanks for the review!")
+					res.redirect("/poetry/" + poem._id);
+				}
+			});
+		}
 	});
+});
 
 //EDIT
-	router.get("/poetry/:id/comments/:comment_id/edit",middleware.isCommentOwner,function(req,res){
-		Poetry.findById(req.params.id,function(err,foundPoetry){
-			if(err)
-				req.flash("error","Something went wrong...Please try again later!");
-			else{
-				Comment.findById(req.params.comment_id,function(err,foundComment){
-				if(err)
-					req.flash("error","Something went wrong...Please try again later!");
-				else{
-					res.render("comments/poetry/edit",{poetry: foundPoetry, comment: foundComment});
-			}
-		});
-			}
-		});
+router.get("/poetry/:id/comments/:comment_id/edit", middleware.isCommentOwner, function (req, res) {
+	Poetry.findById(req.params.id, function (err, foundPoetry) {
+		if (err)
+			req.flash("error", "Something went wrong...Please try again later!");
+		else {
+			Comment.findById(req.params.comment_id, function (err, foundComment) {
+				if (err)
+					req.flash("error", "Something went wrong...Please try again later!");
+				else {
+					res.render("comments/poetry/edit", { poetry: foundPoetry, comment: foundComment });
+				}
+			});
+		}
 	});
+});
 
-	router.put("/poetry/:id/comments/:comment_id",middleware.isCommentOwner,function(req,res){
-		Poetry.findById(req.params.id,function(err,foundPoetry){
-			if(err)
-				req.flash("error","Something went wrong...Please try again later!");
-			else{
-				Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err,updateComment){
-					if(err)
-						req.flash("error","Something went wrong...Please try again later!");
-					else{
-						req.flash("success","Successfully edited your comment :D")
-						res.redirect("/poetry/"+req.params.id);
-					}
-				});
-			}
-		});
+router.put("/poetry/:id/comments/:comment_id", middleware.isCommentOwner, function (req, res) {
+	Poetry.findById(req.params.id, function (err, foundPoetry) {
+		if (err)
+			req.flash("error", "Something went wrong...Please try again later!");
+		else {
+			Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updateComment) {
+				if (err)
+					req.flash("error", "Something went wrong...Please try again later!");
+				else {
+					req.flash("success", "Successfully edited your comment!")
+					res.redirect("/poetry/" + req.params.id);
+				}
+			});
+		}
 	});
+});
 
 //DESTROY
-	router.delete("/poetry/:id/comments/:comment_id",middleware.isCommentOwner,function(req,res){
-		Poetry.findById(req.params.id,function(err,foundPoetry){
-			if(err)
-				req.flash("error","Something went wrong...Please try again later!");
-			else{
-				Comment.findByIdAndRemove(req.params.comment_id,function(err){
-					if(err)
-						req.flash("error","Something went wrong...Please try again later!");
-					else{
-						req.flash("success","Deleted your review! Thanks for adding it in the first place <3")
-						res.redirect("/poetry/"+req.params.id);
-					}
-				});
-			}
-		});
+router.delete("/poetry/:id/comments/:comment_id", middleware.isCommentOwner, function (req, res) {
+	Poetry.findById(req.params.id, function (err, foundPoetry) {
+		if (err)
+			req.flash("error", "Something went wrong...Please try again later!");
+		else {
+			Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+				if (err)
+					req.flash("error", "Something went wrong...Please try again later!");
+				else {
+					req.flash("success", "Deleted your review! Thanks for adding it in the first place!")
+					res.redirect("/poetry/" + req.params.id);
+				}
+			});
+		}
 	});
+});
 
 
 module.exports = router;
--- routes/series/comments.js
+++ routes/series/comments.js
@@ -1,94 +1,94 @@
-var express= require("express");
-var router  = express.Router();
+var express = require("express");
+var router = express.Router();
 var Series = require("../../models/series");
 var Comment = require("../../models/comment");
 var middleware = require("../../middleware");
 
 //NEW
-	router.get("/series/:id/comments/new",middleware.isLoggedIn,function(req,res){
-		Series.findById(req.params.id,function(err,series){
-			if(err)
-				req.flash("error","Something went wrong...Please try again later!");
-			else
-				res.render("comments/series/new",{series : series});
-		});
+router.get("/series/:id/comments/new", middleware.isLoggedIn, function (req, res) {
+	Series.findById(req.params.id, function (err, series) {
+		if (err)
+			req.flash("error", "Something went wrong...Please try again later!");
+		else
+			res.render("comments/series/new", { series: series });
 	});
+});
 
-	router.post("/series/:id/comments",middleware.isLoggedIn,function(req,res){
-		Series.findById(req.params.id,function(err,series){
-			if(err){
-				req.flash("error","Something went wrong...Please try again later!");
-				res.redirect("/series")
-			}
-			else{
-				//create new comment
-				Comment.create(req.body.comment,function(err,comment){
-					if(err)
-						req.flash("error","Something went wrong...Please try again later!");
-					else{
-						comment.author.id = req.user._id;
-						comment.author.username = req.user.username;
-						comment.save();
-						series.comments.push(comment);
-						series.save();
-						req.flash("success","Thanks for the review <3")
-						res.redirect("/series/" + series._id);
-					}
-				});
-			}
-		});
+router.post("/series/:id/comments", middleware.isLoggedIn, function (req, res) {
+	Series.findById(req.params.id, function (err, series) {
+		if (err) {
+			req.flash("error", "Something went wrong...Please try again later!");
+			res.redirect("/series")
+		}
+		else {
+			//create new comment
+			Comment.create(req.body.comment, function (err, comment) {
+				if (err)
+					req.flash("error", "Something went wrong...Please try again later!");
+				else {
+					comment.author.id = req.user._id;
+					comment.author.username = req.user.username;
+					comment.save();
+					series.comments.push(comment);
+					series.save();
+					req.flash("success", "Thanks for the review!")
+					res.redirect("/series/" + series._id);
+				}
+			});
+		}
 	});
+});
 
 //EDIT
-	router.get("/series/:id/comments/:comment_id/edit",middleware.isLoggedIn,function(req,res){
-		Series.findById(req.params.id,function(err,foundSeries){
-			if(err)
-				req.flash("error","Something went wrong...Please try again later!");
-			else{
-				Comment.findById(req.params.comment_id,function(err,foundComment){
-				if(err)
-					req.flash("error","Something went wrong...Please try again later!");
-				else{
-					res.render("comments/series/edit",{series: foundSeries, comment: foundComment});
-			}
-		});
-			}
-		});
+router.get("/series/:id/comments/:comment_id/edit", middleware.isLoggedIn, function (req, res) {
+	Series.findById(req.params.id, function (err, foundSeries) {
+		if (err)
+			req.flash("error", "Something went wrong...Please try again later!");
+		else {
+			Comment.findById(req.params.comment_id, function (err, foundComment) {
+				if (err)
+					req.flash("error", "Something went wrong...Please try again later!");
+				else {
+					res.render("comments/series/edit", { series: foundSeries, comment: foundComment });
+				}
+			});
+		}
 	});
+});
 
-	router.put("/series/:id/comments/:comment_id",middleware.isLoggedIn,function(req,res){
-		Series.findById(req.params.id,function(err,foundSeries){
-			if(err)
-				req.flash("error","Something went wrong...Please try again later!");
-			else{
-				Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err,updateComment){
-					if(err)
-						req.flash("error","Something went wrong...Please try again later!");
-					else{
-						req.flash("success","Successfully edited your comment :D ")
-						res.redirect("/series/"+req.params.id);
-					}
-				});
-			}
-		});
+router.put("/series/:id/comments/:comment_id", middleware.isLoggedIn, function (req, res) {
+	Series.findById(req.params.id, function (err, foundSeries) {
+		if (err)
+			req.flash("error", "Something went wrong...Please try again later!");
+		else {
+			Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updateComment) {
+				if (err)
+					req.flash("error", "Something went wrong...Please try again later!");
+				else {
+					req.flash("success", "Successfully edited your comment!")
+					res.redirect("/series/" + req.params.id);
+				}
+			});
+		}
 	});
+});
 
 //DESTROY
-	router.delete("/series/:id/comments/:comment_id",middleware.isLoggedIn,function(req,res){
-		Series.findById(req.params.id,function(err,foundSeries){
-			if(err)
-				req.flash("error","Something went wrong...Please try again later!");
-			else{
-				Comment.findByIdAndRemove(req.params.comment_id,function(err){
-					if(err)
-						req.flash("error","Something went wrong...Please try again later!");
-					else{
-						req.flash("success","Successfully deleted your review!")
-						res.redirect("/series/"+req.params.id);
-					}
-				});
-			}
-		});
+router.delete("/series/:id/comments/:comment_id", middleware.isLoggedIn, function (req, res) {
+	Series.findById(req.params.id, function (err, foundSeries) {
+		if (err)
+			req.flash("error", "Something went wrong...Please try again later!");
+		else {
+			Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+				if (err)
+					req.flash("error", "Something went wrong...Please try again later!");
+				else {
+					req.flash("success", "Successfully deleted your review!")
+					res.redirect("/series/" + req.params.id);
+				}
+			});
+		}
 	});
+});
 
 module.exports = router;
